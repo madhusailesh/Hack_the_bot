030f0fbfdc3fd6bc1e1e0543456d182e18d5ad76
piyushmishra06 <piyushmishra0620@gmail.com>
2026-01-08 22:19:08 +0530

    Added the userId for extra identification and ease of allocation among contestants

--- hack-the-bot/src/app/services/auth/route.ts
+++ hack-the-bot/src/app/services/auth/route.ts
@@ -8,19 +8,21 @@ interface User{
 
 export async function POST(req:NextRequest){
     const {name,regd_no} : User = await req.json();
+    const uId : string = "User"+(Math.floor(Math.random()*100000)).toString();
     if(!name || !regd_no){
         return NextResponse.json({status:400,message:"All the credentials are required."});
     }
     try{
         const clientPromise = await client;
         const db = clientPromise.db("hackthebot");
+        await db.collection("users").createIndex({regd_no:1},{unique:true});
         const users = db.collection("users");
         const user = await users.findOne({regd_no:regd_no});
         if(user){
             return NextResponse.json({status:401,message:"Credential exists"});
         }
-        const response = await users.insertOne({name:name,regd_no:regd_no});
-        return NextResponse.json({status:200,message:"Successful registration"});
+        const response = await users.insertOne({name:name,regd_no:regd_no,uId:uId});
+        return NextResponse.json({status:200,message:"Successful registration",uId:uId});
     }catch(Err:any){
         console.error(Err);
         return NextResponse.json({status:Err.status,error:Err.message});
