96da85ad6415124f0cd4b7b585d7afa444b4e21a
madhusailesh <madhusaileshsasamal998@gmail.com>
2026-01-08 17:46:08 +0530

    Add error handling for missing MONGODB_URI environment variable

--- hack-the-bot/src/lib/db.ts
+++ hack-the-bot/src/lib/db.ts
@@ -6,6 +6,10 @@ declare global{
 
 const uri = process.env.MONGODB_URI; 
 
+if (!uri) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+}
+
 if(!(global.mongoClient)){
     const conn = new MongoClient(uri);
     global.mongoClient = conn.connect();
